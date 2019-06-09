e44658f4673c9d1b4e96b533b0a2373f685bfca3
Ebrahiem-Gamal-Mohamed <e.g.mhmd2020@gmail.com>
2019-06-09 13:17:48 +0200

    add pwa service and implement the pwa module

--- new-offline-app/src/app/app.component.ts
+++ new-offline-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { PwaServiceService } from './pwa-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'new-offline-app';
+
+  constructor(private swUpdate: SwUpdate, public Pwa: PwaServiceService) {
+    swUpdate.available.subscribe(event => {
+      if (event) {
+        this.swUpdate.activateUpdate().then(() => window.location.reload());
+      }
+    });
+  }
+
+  installPwa(): void {
+    this.Pwa.promptEvent.prompt();
+  }
+
 }
--- new-offline-app/src/app/app.module.ts
+++ new-offline-app/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+//Services ...
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { PwaServiceService } from './pwa-service.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
+  providers: [PwaServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
